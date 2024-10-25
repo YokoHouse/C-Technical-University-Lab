a996812dc3787010bb13d68b5a8a6240459eec6c
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-10-25 11:07:49 +0300

    Update main.cpp

--- наследяване/main.cpp
+++ наследяване/main.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Person{
+    protected:
+        int age;
+        string name;
+    public:
+        Person(string name, int age):age(age), name(name){}
+        
+        void aboutMe() {
+            cout << "IAM " << name << ".Iam " << age << " years old." << endl;
+        }
+
+};
+
+class Worker : public Person{
+    protected:
+        int exp;
+    public:
+    Worker(string name, int age, int exp):Person(name, age), exp(exp) {}
+
+    void aboutMe() {
+            Person::aboutMe();
+            cout << "IAM working for " << exp << " years." << endl;
+        }
+};
+
+int main(){
+
+
+    Worker w("Georgi", 25, 3);
+    w.aboutMe();
+    return 0;
+
+}
